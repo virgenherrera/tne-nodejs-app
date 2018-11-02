a7967566db72abafd6524ac05b9479d00446d6b5
Hugo Virgen <virgenherrera@gmail.com>
2018-11-01 19:30:46 -0600

    changelog
    - debug exceptionHandler

--- src/lib/exceptionHandler.ts
+++ src/lib/exceptionHandler.ts
@@ -1,5 +1,4 @@
 import * as StackTrace from 'stacktrace-js';
-import { parse } from 'path';
 import { Defaults } from '../constant/defaults';
 
 export function bindExceptionHandler(appName: string = null, fatalErrHandler = null, logger: any = console): void {
@@ -19,37 +18,22 @@ export function bindExceptionHandler(appName: string = null, fatalErrHandler = n
 }
 
 export function ExceptionHandler(appName: string = Defaults.appName, logger: any = console) {
-	return async function (E) {
+	return async function onException(E) {
 
 		logger.error(`${appName} has Crashed!`);
 
 		try {
 			const stackFrames = await StackTrace.fromError(E);
-			const stackStr = stackFrames.reduce((acc, sf, i) => {
-				const { name, ext } = parse(sf.fileName);
-
-				acc += (!acc) ? '\t"Stack Trace"\n' : '\n';
-				acc += `stackIndex: ${i}\n`;
-				acc += `file: ${name + ext}\n`;
-				acc += `function: ${sf.functionName.replace('Object.', '')}\n`;
-				acc += `line: ${sf.lineNumber}\n`;
-				acc += `column: ${sf.columnNumber}\n`;
-				acc += `at: ${sf.source.replace(/(\s+at\s)/, '')}\n`;
-
-				return acc;
-			}, '');
 
 			if (E.name && E.message) {
 				logger.error(`${E.name}:${E.message}`);
 			}
 
-			logger.error(stackStr);
+			logger.error(JSON.stringify(stackFrames, null, 3));
 			logger.warn('Exiting process!');
 
 		} catch (E) {
 			logger.error(E);
 		}
-
-		process.exit(1);
 	};
 }
